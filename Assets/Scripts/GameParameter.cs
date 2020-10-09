ae503eb96d8a1cc94f1e8874363d78355addf313
momomorgentau <sslmatsui0214@gmail.com>
2020-10-10 02:33:29 +0900

    createneedle

---
+++ Assets/Scripts/GameParameter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameParameter", menuName = "GameParameter")]
+public class GameParameter : ScriptableObject
+{
+    public float xMax;
+    public float xMin;
+    public float zMax;
+    public float zMin;
+
+}
---
+++ Assets/Scripts/GroundManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Groundを管理（タップ）
+public class GroundManager : MonoBehaviour
+{
+    //関数登録（針を降らす）
+    Action tapAction; //クリックされたときに実行したい関数
+
+    //tapActionに関数を登録する関数
+    public void AddEventListenerOnTap(Action action)
+    {
+        tapAction += action;
+    }
+
+    public void OnTap() 
+    {
+        tapAction();
+    }
+}
